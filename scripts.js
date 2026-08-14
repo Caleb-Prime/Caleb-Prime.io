11d3678bb5f06b3f42c7bfc918103ae55726a394
Caleb-Prime <griff.code@proton.me>
2026-08-14 21:01:38 +0700

    Implement mousemove trail effect
    Add mousemove event listener to create trailing dots.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+document.addEventListener("mousemove", (e) => {
 
+    for (let i = 0; i < 2; i++) {
+
+        const dot = document.createElement("div");
+        dot.className = "trail";
+
+        dot.style.left = (e.clientX + (Math.random()*12-6)) + "px";
+        dot.style.top = (e.clientY + (Math.random()*12-6)) + "px";
+
+        document.body.appendChild(dot);
+
+        setTimeout(() => {
+            dot.remove();
+        }, 600);
+
+    }
+
+});
